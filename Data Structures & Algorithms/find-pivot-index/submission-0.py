548c87b3970d443d55e178ee04f0b87e0eb7a6c6
Geetpal Singh <geetpal.gs@gmail.com>
2026-05-20 15:15:34 -0400

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        totalSum = sum(nums)
+
+        leftSum = 0
+
+        for i in range(len(nums)):
+            
+            rightSum = totalSum - leftSum - nums[i]
+
+            if rightSum == leftSum:
+                return i
+            
+            leftSum += nums[i]
+        
+        return -1
